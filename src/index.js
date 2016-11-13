3fe3cc6204795030b6706053cd975a7d7a12ff18
Pavel Skritskiy <pavel_working@mail.ru>
2016-11-13 11:13:52 +0000

    task 2A

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/task2A', function (req, res) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    let result = (parseInt(req.query.a, 10) || 0) + (parseInt(req.query.b, 10) || 0);
+    res.send(String(result));
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
