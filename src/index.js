890b37cada543ee4c25011e320a1a74498b0355b
Pavel Skritskiy <pavel_working@mail.ru>
2016-11-13 21:00:16 +0000

    task 2B

--- src/index.js
+++ src/index.js
@@ -14,6 +14,51 @@ app.get('/task2A', function (req, res) {
     res.send(String(result));
 });
 
+app.get('/task2B', function (req, res) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    let result;
+
+    const capitalFirst = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    };
+
+    if (req.query.fullname) {
+        const words = req.query.fullname.trim().split(/\s+/);
+
+        if (words.length > 0 && words.length <= 3) {
+            result = [];
+            for (let i = 0; i < words.length; i++) {
+                if (words[i].match(/[0-9\~\!\@\#\$\%\^\&\*\(\)\_\+\=\-\[\]\{\}\;\:\"\\\/\<\>\?]/) ||
+                    words[i].toLowerCase().match(/([a-z][а-я]|[а-я][a-z])/)
+                ) {
+                    result = 'Invalid fullname';
+                    break;
+                } else {
+                    if (i === words.length - 1) {
+                        result.unshift(capitalFirst(words[i]));
+                    } else {
+                        result.push(words[i].charAt(0).toUpperCase() + '.');
+                    }
+                }
+            }
+
+            if (Array.isArray(result)) {
+                result = result.join(' ');
+            }
+
+        } else {
+            result = 'Invalid fullname';
+        }
+    } else {
+        result = 'Invalid fullname';
+    }
+
+    res.send(String(result));
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
