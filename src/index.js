30af1e625699d61949b493081b3fed7a98cb515c
Pavel Skritskiy <pavel_working@mail.ru>
2016-11-20 21:21:33 +0000

    task 2D

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import _ from 'lodash';
 
 import task3A from './task3A';
 
@@ -76,6 +77,127 @@ app.get('/task2C', function (req, res) {
 });
 
 app.use('/task3A', task3A);
+
+app.get('/tash2D', (req, res) => {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    function hueToRgb(p, q, t) {
+        if (t < 0) {
+            t += 1;
+        }
+        if (t > 1) {
+            t -= 1;
+        }
+        if (t < 1 / 6) {
+            return p + ( q - p ) * 6 * t;
+        }
+        if (t < 1 / 2) {
+            return q;
+        }
+        if (t < 2 / 3) {
+            return p + ( q - p ) * ( ( 2 / 3 - t ) * 6 );
+        }
+
+        return p;
+    }
+
+    function hslToRgb(bits) {
+        let rgba = {};
+        const hsl = {
+            h: bits[1] / 360,
+            s: bits[2] / 100,
+            l: bits[3] / 100
+        };
+
+        if (hsl.s > 1) {
+            return '';
+        }
+
+        if (hsl.s === 0) {
+            let v = Math.round(255 * hsl.l);
+            rgba = {
+                r: v,
+                g: v,
+                b: v
+            };
+        } else {
+            let q = hsl.l < 0.5 ? hsl.l * ( 1 + hsl.s ) : ( hsl.l + hsl.s ) - ( hsl.l * hsl.s );
+            let p = 2 * hsl.l - q;
+            rgba.r = Math.round(hueToRgb(p, q, hsl.h + ( 1 / 3 )) * 255, 10);
+            rgba.g = Math.round(hueToRgb(p, q, hsl.h) * 255, 10);
+            rgba.b = Math.round(hueToRgb(p, q, hsl.h - ( 1 / 3 )) * 255, 10);
+        }
+
+        rgba = {
+            r: Number(rgba.r).toString(16),
+            g: Number(rgba.g).toString(16),
+            b: Number(rgba.b).toString(16)
+        };
+
+        if (rgba.r.length === 1) {
+            rgba.r = '0' + rgba.r;
+        }
+        if (rgba.g.length === 1) {
+            rgba.g = '0' + rgba.g;
+        }
+        if (rgba.b.length === 1) {
+            rgba.b = '0' + rgba.b;
+        }
+
+        return rgba.r + rgba.g + rgba.b;
+    }
+
+    let result = '';
+
+    if (req.query.color) {
+        let matches;
+
+        try {
+            let color = decodeURIComponent(req.query.color).trim();
+
+            if (color.match(/^#?[a-f0-9A-F]{3,6}$/)) {
+                if (color.charAt(0) === '#') {
+                    color = color.slice(1);
+                }
+
+                if (color.length === 3 || color.length === 6) {
+                    if (color.length === 3) {
+                        color = color.charAt(0) + color.charAt(0)
+                            + color.charAt(1) + color.charAt(1)
+                            + color.charAt(2) + color.charAt(2);
+                    }
+
+                    result = color.toLowerCase();
+                }
+            } else if (matches = color.match(/^rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)$/i)) {
+                for (const i of [1, 2, 3]) {
+                    const n = Number(matches[i]);
+                    if (n >= 0 && n <= 255) {
+                        const s = n.toString(16);
+                        if (s.length === 1) {
+                            result += '0';
+                        }
+                        result += s;
+                    } else {
+                        result = '';
+                        break;
+                    }
+                }
+            }
+        } catch (e) {
+            let color = req.query.color.trim().replace(/%20/g, '');
+            console.log(color);
+
+            if (matches = color.match(/^hsl\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%\)$/)) {
+                result = hslToRgb(matches);
+            }
+        }
+    }
+
+    res.send(result ? '#' + result : 'Invalid color');
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
